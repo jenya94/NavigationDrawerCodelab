1025d10d3da26c1734b4fde36039492afa28e139
jenisbay <jenis.q94@gmail.com>
2023-01-06 15:16:27 +0500

    Initial commit

---
+++ app/src/main/java/com/example/navigationdrawercodelab/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.navigationdrawercodelab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
